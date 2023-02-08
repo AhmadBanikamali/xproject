21301cc5c233a11e4aeda974ef1e5b10dbb8a2ff
ahmad.banikamali1 <ahmad.banikamali.git@gmail.com>
2023-02-08 09:12:02 +0330

    Initial commit

---
+++ app/src/main/java/com/abcdandroid/xproject/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.abcdandroid.xproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// solid
+// navigation
+// pagination
+// dependency injection
+// room
+// hilt
+// coroutine
+// mvvm
+// solid
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
